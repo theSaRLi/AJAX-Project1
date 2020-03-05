421dbb82a55fa57245e9c0f8c47fda051b8b463b
Sara <fischersara136@gmail.com>
2020-03-05 15:14:24 +0100

    initial commit

---
+++ doggos.js
@@ -0,0 +1,18 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random"; 
+
+const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".doggos");  
+
+promise 
+    .then(function(response) {
+        const processingPromise = response.json(); 
+        return processingPromise; 
+    })
+    .then(function(processedResponse) {
+        const img = document.createElement("img"); 
+        img.src = processedResponse.message; 
+        img.alt = "Cute dog"; 
+        doggos.appendChild(img); 
+    }); 
+
+console.log("this will log first"); 
